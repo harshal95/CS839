b5b7c9ae8dba8e3a13aff51d7442feffd4a8978d
Shebin Roy <rshebin18@gmail.com>
2019-04-10 19:27:46 -0500

    Order of field names

--- stage_2/code/cs839/cs839/spiders/RottonTom_spider.py
+++ stage_2/code/cs839/cs839/spiders/RottonTom_spider.py
@@ -3,7 +3,7 @@ import scrapy
 
 
 class RottombotSpider(scrapy.Spider):
-	name = 'RottenTomatoes'
+	name = 'RottonTomatoes'
 	allowed_domains = ['www.rottentomatoes.com']
 	#start_urls = ['https://www.rottentomatoes.com/top/bestofrt/?year=2018']
 
@@ -12,41 +12,55 @@ class RottombotSpider(scrapy.Spider):
 		'https://www.rottentomatoes.com/top/bestofrt/?year='
 		]
 		for url in urls:
+			year = 1980
 			for year in range(1910, 2020):
 				current_url = url + str(year)
 				yield scrapy.Request(url=current_url, callback=self.parse)
 
 	def parse_movie_info(self, response):
 		data = dict()
-		data['title'] = response.xpath('//*[@id="topSection"]/div[2]/div[1]/h1/text()').get()
+		name = response.xpath('//*[@id="topSection"]/div[2]/div[1]/h1/text()').get()
 		movie_info = response.xpath('//*[@id="mainColumn"]/section[3]/div/h2/text()').get()
 		if movie_info == "Movie Info":
 			movie_info = response.xpath('//*[@id="mainColumn"]/section[3]/div/div')
 		else:
 			movie_info = response.xpath('//*[@id="mainColumn"]/section[2]/div/div')
 
-		data['genre'] = movie_info.xpath('ul/li[2]/div[2]/a/text()').getall()
-		data['director'] = movie_info.xpath('ul/li[3]//a/text()').getall()
-		data['rating'] = response.xpath('//*[@id="js-rotten-count"]/text()').getall()[-1].replace("\n", "").replace(" ", "")
+		genre = movie_info.xpath('ul/li[2]/div[2]/a/text()').getall()
+		director = movie_info.xpath('ul/li[3]//a/text()').getall()
+		rating = response.xpath('//*[@id="js-rotten-count"]/text()').getall()[-1].replace("\n", "").replace(" ", "")
 
+		release = ""
+		runtime = ""
+		grossing = ""
+		runtime = ""
+		certificate = ""
 		i = 1
 		while True:
 			tag = movie_info.xpath('ul/li[' + str(i) + ']/div[1]/text()').get()
 			if tag is not None:
+				if tag == "Rating: ":
+					certificate = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/text()').get().split(" ")[0]
 				if tag == "In Theaters: ":
-					data['release'] = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/time/text()').get()
+					release = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/time/text()').get()
 				if tag == "Runtime: ":
-					data['runtime'] = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/time/text()').get()
+					runtime = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/time/text()').get()
 				if tag == "Box Office: ":
-					data['grossing'] = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/text()').get()
+					grossing = movie_info.xpath('ul/li[' + str(i) + ']/div[2]/text()').get()
 					#print("\n\n\n\nFOUND FOUND FOUND\n\n\n\n")
 			i = i + 1
 			if i > 10:
 				break
-		if 'runtime' not in data:
-			data['runtime'] = ''
-		data['runtime'] = data['runtime'].replace("\n", "").replace("  ", "")
-		
+		runtime = runtime.replace("\n", "").replace("  ", "")
+		data['Name'] = name
+		data['ReleaseDate'] = release
+		data['Certificate'] = certificate
+		data['Rating'] = rating
+		data['Runtime'] = runtime
+		data['Director'] = director
+		data['Genre'] = genre
+		data['Grossing'] = grossing
+        
 		#tag = movie_info.xpath('ul/li[7]/div[1]/text()').get()
 		#print("|" + tag + "|")
 		#if tag == "Runtime: ":
